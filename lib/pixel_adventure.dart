3692cb4a8185479c01e12be74dd541c207332590
JuanDavilaM <juanpadavila7781@gmail.com>
2025-06-03 08:56:54 -0500

    phone joystick

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/painting.dart';
 import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
@@ -16,6 +17,7 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents, DragCa
   late final CameraComponent cam;
   Player player = Player(character: 'Mask Dude');
   late JoystickComponent joystick;
+  bool showJoystick = false;
 
   
 
@@ -42,10 +44,55 @@ class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents, DragCa
     return super.onLoad();
   }
   
+  @override
+  void update(double dt) {
+    if(showJoystick){
+      updateJoystick();
+    }
+    
+    super.update(dt);
+  }
   void addJoystick() {
     joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(images.fromCache('HUD/knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(images.fromCache('HUD/Joystick.png')),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
       
     );
+    if(showJoystick) {
+          add(joystick);
 
-}
+    }
+
+  }
+  
+  void updateJoystick() {
+
+    switch(joystick.direction) {
+
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
+
+
+
+  }
+
+
 }
