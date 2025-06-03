5b70efad31a3f50f2e05c0cc9fdebc1a92ffb688
JuanDavilaM <juanpadavila7781@gmail.com>
2025-06-03 11:24:22 -0500

    movimiento del personaje y hitbox

---
+++ lib/components/player_hitbox.dart
@@ -0,0 +1,9 @@
+class PlayerHitbox {
+  final double offsetX;
+  final double offsetY; 
+  final double width;
+  final double height;
+
+  PlayerHitbox({
+    required this.offsetX, required this.offsetY, required this.width, required this.height});
+}
