cbb495fa74f331c37946bbf3d676feb3336f26b7
JuanDavilaM <juanpadavila7781@gmail.com>
2025-06-03 07:42:35 -0500

    Primer commit del proyecto pixel_adventure

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/actors/player.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents, DragCallbacks {
+
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+
+
+  late final CameraComponent cam;
+  Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
+
+  
+
+@override
+
+  Future<void> onLoad() async {
+ //load all images en el cash
+    await images.loadAllImages();
+
+    final world = Level(
+    player: player, 
+    levelName: ('Level_01')
+  );
+
+    cam = CameraComponent.withFixedResolution(world: world, width: 640, height: 360 );
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+
+    addAll([cam, world]);
+
+
+    addJoystick();
+
+    return super.onLoad();
+  }
+  
+  void addJoystick() {
+    joystick = JoystickComponent(
+      
+    );
+
+}
+}
